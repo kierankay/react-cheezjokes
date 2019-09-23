3f89fe62b965478593769c60bc1780361f9ed427
Kieran Kay <kierankay@gmail.com>
2019-09-23 15:17:45 -0700

    finished everything before further study

---
+++ src/JokesContainer.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import axios from 'axios';
+import './JokesContainer.css'
+import VoteButton from './VoteButton';
+import Joke from './Joke';
+import VoteCount from './VoteCount';
+
+class JokesContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: [
+        {
+          joke: '',
+          votes: 0
+        }
+      ],
+      loading: true
+    }
+    this.handleVote = this.handleVote.bind(this);
+  }
+
+  static defaultProps = {
+    numJokes: 10
+  }
+
+  async componentDidMount() {
+    let result = await axios.get(`https://icanhazdadjoke.com/search?limit=${this.props.numJokes}`,
+      { headers: { Accept: "application/json" } });
+    let jokesWithVotes = result.data.results.map((joke, idx) => {
+      joke.votes = 0;
+      return joke;
+    })
+    this.setState({ jokes: jokesWithVotes, loading: false });
+  }
+
+  handleVote(value, idx) {
+    let editedJokes = this.state.jokes.map((joke, jIdx) => {
+      return (jIdx === idx ? { ...joke, votes: joke.votes + value } : joke)
+    });
+    this.setState({ jokes: editedJokes });
+  }
+
+  render() {
+    return (
+      this.state.loading ?
+        <div className="loading">Loading...</div> :
+      this.state.jokes.map((joke, idx) => {
+        return (
+          // add stylesheet
+          <div className="container" key={joke.id}>
+            <div className="joke-container">
+              <div className="votes-container">
+                <VoteButton icon="⬆" value={1} idx={idx} action={this.handleVote} />
+                <VoteCount voteCount={joke.votes} />
+                <VoteButton icon="⬇" value={-1} idx={idx} action={this.handleVote} />
+              </div>
+              <div>
+                <Joke jokeText={joke.joke} />
+              </div>
+            </div>
+          </div>
+        )
+      })
+    )
+  }
+}
+
+export default JokesContainer;
